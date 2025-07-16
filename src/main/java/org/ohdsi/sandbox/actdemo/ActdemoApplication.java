6d450de89a211c155588b87f8b06ae668c61878b
David Mishler <profmish@gmail.com>
2025-07-16 11:32:16 -0400

    Create skeleton project

---
+++ src/main/java/org/ohdsi/sandbox/actdemo/ActdemoApplication.java
@@ -0,0 +1,13 @@
+package org.ohdsi.sandbox.actdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ohdsi/sandbox/actdemo/ActdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohdsi.sandbox.actdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
