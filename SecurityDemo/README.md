7eb509ce0b92ea57b5be198a0918ede11fffc447
David Mishler <profmish@gmail.com>
2025-08-10 16:47:19 -0400

    security wip

--- SecurityDemo/README.md
+++ SecurityDemo/README.md
@@ -1,2 +1,24 @@
 # WebAPI Security Demo
 
+
+
+## Password Encoding
+
+Starting with Spring 5, a new "delegating" password encoder is used to encode passwords.  This works by placing a prefix in front of the password value that indicates the encoding type.  Very secure:
+
+* {bcrypt} - uses `BCryptPasswordEncoder`
+* {pbdkf2} - uses `Pbdkf2PasswordEncoder`
+* {scrypt} - uses `ScryptPasswordEncoder`
+* {argon2} - uses `Argon2PasswordEncoder`
+
+Less secure and/or legacy applications (such as current WebAPI) use approaches that are generally recognized as not secure:
+
+* {noop} - Uses `NoOpPasswordEncoder` - no encryption is done.  Useful for testing.  Not for production.
+* {sha256} - uses `StandardPasswordEncoder`
+* {md5} - uses `MessageDigestPasswordEncoder`
+* lots more...
+
+*Note*: moving to a more secure password encryption may require some work on our part to *try* to upgrade existing encoded values, or may require that users provide new passwords.
+
+
+
---
+++ SecurityDemo/src/main/java/org/ohdsi/sandbox/secdemo/SecDemoSecurityConfig.java
@@ -0,0 +1,60 @@
+package org.ohdsi.sandbox.secdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecDemoSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Two users.  Shows password encoder patterns {noop} - no encoding,
+        // and {bcrypt} which the current state of the art
+        var user = User.withUsername("app1").password("{noop}ohdsi").authorities("read").build();
+
+        // Since this is a demo, password is Super-Duper-85$
+        var admin = User.withUsername("admin")
+                            .password("{bcrypt}$2a$12$imtbrq3VEa0ZPLAnhpCZHeg4Z8Kjj4Cn9kGhyFRGdesEzQMwvvMwK")
+                            .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    // The delegating password encoder is new to Spring 5.  It uses the prefix to find
+    // the appropriate encoder.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // From Spring Security 6.3 version.  Probably not for production,
+    // but interesting that we could add a notice to the user that their
+    // password is not so good.  Would also only interoperate with in memory
+    // and auth db situations, with OAUTH we're out of the loop.
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
