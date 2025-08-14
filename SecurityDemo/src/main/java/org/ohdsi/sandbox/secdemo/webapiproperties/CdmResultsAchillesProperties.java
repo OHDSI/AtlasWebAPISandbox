5afa40eac77a70eb855854b9845036e35df2f69c
David Mishler <profmish@gmail.com>
2025-08-14 10:02:42 -0400

    Simplify properties

--- SecurityDemo/src/main/java/org/ohdsi/sandbox/secdemo/webapiproperties/CdmResultsAchillesProperties.java
+++
@@ -1,9 +0,0 @@
-package org.ohdsi.sandbox.secdemo.webapiproperties;
-
-public class CdmResultsAchillesProperties {
-    Boolean enableCacheWarming;
-
-    public Boolean getEnableCacheWarming() {return enableCacheWarming;}
-    public Boolean enableCacheWarming() {return enableCacheWarming;}
-    public void setEnableCacheWarming(Boolean enableCacheWarming) {this.enableCacheWarming = enableCacheWarming;}
-}
--- SecurityDemo/src/main/java/org/ohdsi/sandbox/secdemo/webapiproperties/CdmResultsConceptRecordProperties.java
+++
@@ -1,9 +0,0 @@
-package org.ohdsi.sandbox.secdemo.webapiproperties;
-
-public class CdmResultsConceptRecordProperties {
-    Boolean usePersonCount;
-
-    public Boolean getUsePersonCount() {return usePersonCount;}
-    public Boolean usePersonCount() {return usePersonCount;}
-    public void setUsePersonCount(Boolean usePersonCount) {this.usePersonCount = usePersonCount;}
-}
