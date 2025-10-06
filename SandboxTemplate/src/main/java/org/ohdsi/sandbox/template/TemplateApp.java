f1c85e5f58f37b0578e3f6fc08d1e40e592be52e
Chris Knoll <cknoll@ohdsi.org>
2025-10-06 11:29:05 -0400

    Initial Commit of Sandbox Template assets.

---
+++ SandboxTemplate/src/main/java/org/ohdsi/sandbox/template/TemplateApp.java
@@ -0,0 +1,18 @@
+package org.ohdsi.sandbox.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TemplateApp extends SpringBootServletInitializer { 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TemplateApp.class);
+	}	
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApp.class, args);
+	}
+}
